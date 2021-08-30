d9878e102d5caaf7c97568d6e9738a0b2e1f7b7c
MrRottenCraft <89740593+MrRottenCraft@users.noreply.github.com>
2021-08-30 09:10:42 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -60,3 +60,27 @@ addLayer("p", {
     },
 })
 
+addLayer("r", {
+    name: "Rubens", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+
+    color: "#FF0000",
+    requires: new Decimal(5), // Can be a function that takes requirement increases into account
+    resource: "Rubens", // Name of prestige currency
+    baseResource: "Points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.333, // Prestige currency exponent
+
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+    },
+
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "r", description: "A: Reset for getting Apples kinda faster.", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+})
