954d690259ef38830f527de063d3d01073aea258
MrRottenCraft <89740593+MrRottenCraft@users.noreply.github.com>
2021-08-31 09:37:20 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -7,8 +7,8 @@ addLayer("p", {
 		points: new Decimal(0),
     }},
     color: "#blue",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    requires: new Decimal(5), // Can be a function that takes requirement increases into account
+    resource: "Rubens", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -30,25 +30,25 @@ addLayer("p", {
     upgrades: {
 
         11: {
-            name: "The first upgrade!",
-            description: "Add .5 to your base.",
-            cost: new Decimal(1),
+            name: "1",
+            description: "Point gain x4.",
+            cost: new Decimal(2),
         },
 
         12: {
-            name: "Yes",
-            description: "Be Pro.",
-            cost: new Decimal(2),
+            name: "2",
+            description: "Lands boost Point gain.",
+            cost: new Decimal(5),
 
             effect() {
-                return player[this.layer].points.add(1).pow(0.5)
+                return player[this.layer].points.add(3).pow(0.562)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },
 
         13: {
-            name: "Yes",
-            description: "Be Pro.",
+            name: "3",
+            description: "Points boost themself",
             cost: new Decimal(5),
 
             effect() {
@@ -57,20 +57,61 @@ addLayer("p", {
 
         },
 
+        14: {
+            name: "4",
+            description: "Points boost Rubens gain.",
+            cost: new Decimal(60),
+
+            effect() {
+                return player.points.add(2).pow(0.2)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, 
+
+        },
+
     },
 }),
 
-addLayer("r", {
+addLayer("P", {
     startData() { return {                  // startData is a function that returns default data for a layer. 
         unlocked: false,                     // You can add more variables here to add them to your layer.
         points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
     }},
 
     color: "#4BDC13",                       // The color for this layer, which affects many elements.
-    resource: "Rubens",            // The name of this layer's main prestige resource.
+    resource: "Pubens",            // The name of this layer's main prestige resource.
     row: 1,                                 // The row this layer is on (0 is the first row).
 
-    baseResource: "prestige points",                 // The name of the resource your prestige gain is based on.
+    baseResource: "Rubens",                 // The name of the resource your prestige gain is based on.
+    baseAmount() { return player.p.points },  // A function to return the current amount of baseResource.
+
+    requires: new Decimal(100000),              // The amount of the base needed to  gain 1 of the prestige currency.
+                                            // Also the amount required to unlock the layer.
+
+    type: "normal",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
+
+    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
+        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
+    },
+    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
+        return new Decimal(1)
+    },
+
+    layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
+})  
+
+addLayer("a", {
+    startData() { return {                  // startData is a function that returns default data for a layer. 
+        unlocked: false,                     // You can add more variables here to add them to your layer.
+        points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
+    }},
+
+    color: "#4BDC13",                       // The color for this layer, which affects many elements.
+    resource: "Achievements",            // The name of this layer's main prestige resource.
+    row: side,                                 // The row this layer is on (0 is the first row).
+
+    baseResource: "Achievements",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.p.points },  // A function to return the current amount of baseResource.
 
     requires: new Decimal(10),              // The amount of the base needed to  gain 1 of the prestige currency.
@@ -87,4 +128,11 @@ addLayer("r", {
     },
 
     layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
+
+    achievements: {
+        11: {
+            name: "Blah",
+        },
+        etc
+    }
 })  
